42145a2509074e3e7278972dc03677a75e39cc84
Denys <denis_2007@windowslive.com>
2021-08-18 23:01:15 +0300

    ff

---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+using Random = System.Random;
+
+public class Test : MonoBehaviour
+{
+   private void Start()
+   {
+      //GameRoot.Instance.CurrentLevel--;
+   }
+}
